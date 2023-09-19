1463d3db9315f3dc040642e34922603bc6533e8b
Gonzalo <gonzaloamaya@tdf.edu.ar>
2023-09-19 16:08:45 -0300

    creación de entity profesor

--- src/profesor/entities/profesor.entity.ts
+++ src/profesor/entities/profesor.entity.ts
@@ -1,1 +1,39 @@
-export class Profesor {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: 'profesor' })
+export class Profesor {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    nombre: string;
+
+    @Column()
+    apellido: string;
+
+    constructor(nombre: string, apellido: string){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public getId(): number{
+        return this.id;
+    }
+
+    public getNombre(): string{
+        return this.nombre;
+    }
+
+    public getApellido(): string{
+        return this.apellido;
+    }
+
+    public setNombre(nombre: string): void{
+        this.nombre = nombre;
+    }
+    
+    public setApellido(apellido: string):void{
+        this.apellido = apellido;
+    }
+}
