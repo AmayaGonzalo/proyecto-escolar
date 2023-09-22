87e4687e2e20f176663076f782367d5543d68eca
Gonzalo <gonzaloamaya@tdf.edu.ar>
2023-09-22 02:23:18 -0300

    agregado en estudiantesServir Crear

--- src/estudiante/estudiante.service.ts
+++ src/estudiante/estudiante.service.ts
@@ -1,12 +1,32 @@
-import { Injectable } from '@nestjs/common';
-import { CreateEstudianteDto } from './dto/create-estudiante.dto';
-import { UpdateEstudianteDto } from './dto/update-estudiante.dto';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { EstudianteDto } from './dto/create-estudiante.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Estudiante } from './entities/estudiante.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class EstudianteService {
-  create(createEstudianteDto: CreateEstudianteDto) {
-    return 'This action adds a new estudiante';
-  }
+
+  constructor(@InjectRepository(Estudiante)
+              private estudianteRepository:Repository<Estudiante>)
+  {}
+
+  async create(estudianteDto: EstudianteDto): Promise<EstudianteDto> {   
+    try{
+      const estudiante: Estudiante = await this.estudianteRepository.save(new Estudiante(estudianteDto.nombre, estudianteDto.apellido, estudianteDto.fechaNacimiento))
+        if(estudiante)
+          return estudianteDto;
+        else
+          throw new Error('No se pudo crear el estudiante');
+      }        
+      catch(error){
+        throw new HttpException({
+            status: HttpStatus.CONFLICT,
+            error: 'Error en estudiante - ' + error
+  
+        },HttpStatus.NOT_FOUND)
+      }  
+    }  
 
   findAll() {
     return `This action returns all estudiante`;
@@ -16,7 +36,7 @@ export class EstudianteService {
     return `This action returns a #${id} estudiante`;
   }
 
-  update(id: number, updateEstudianteDto: UpdateEstudianteDto) {
+  update(id: number, estudianteDto: EstudianteDto) {
     return `This action updates a #${id} estudiante`;
   }
 
